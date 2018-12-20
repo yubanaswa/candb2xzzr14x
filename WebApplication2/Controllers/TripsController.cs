18652d2ae2658cf734fa8565bdf5e6eaca44817c
Yubanaswa Chakraborty <yubanaswa@hotmail.com>
2018-12-20 11:54:50 +0000

    Add project files.

---
+++ WebApplication2/Controllers/TripsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class TripsController : Controller
+    {
+        // GET: Trips
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
