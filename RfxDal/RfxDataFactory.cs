9e9e94663ad2d760d7ffe25d3e1a29523c433911
Yuban <Yuban@DESKTOP-0CD5VTD>
2019-01-06 15:37:03 +0000

    unit test project added

---
+++ RfxDal/RfxDataFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RfxDal.DTO;
+
+namespace RfxDal
+{
+
+    public static class RfxDataFactory
+    { 
+        public static List<Trips> GetAllTrips()
+        {
+            var trips = new List<Trips>();
+            using (var context = new candb_webEntities())
+            {
+                trips = context.Trips.Select(p => p).ToList();
+            }
+
+            return trips;
+        }
+    }
+}
---
+++ RfxDalTests/RfxDataFactoryTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RfxDal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RfxDal.Tests
+{
+    [TestClass()]
+    public class RfxDataFactoryTests
+    {
+        [TestMethod()]
+        public void GetAllTripsTest()
+        {
+            var trips = RfxDataFactory.GetAllTrips();
+            Assert.IsNotNull(trips);
+        }
+    }
+}
