f9379ea471923a20c0b2b43554e87bddcaa97560
Yuban <Yuban@DESKTOP-0CD5VTD>
2019-01-06 14:09:31 +0000

    DAL added. Page content added

--- WebApplication2/Controllers/TripsController.cs
+++ WebApplication2/Controllers/TripsController.cs
@@ -9,9 +9,21 @@ namespace WebApplication2.Controllers
     public class TripsController : Controller
     {
         // GET: Trips
-        public ActionResult Index()
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult Index(string p)
         {
+            ViewBag.DisplayName = p;
             return View();
+        }
+
+        //[Route("Trips/SearchTrip/{searchParam}")]
+        public ActionResult SearchTrip(string searchParam)
+        {
+            return View("SearchTripResults");
         }
     }
 }
